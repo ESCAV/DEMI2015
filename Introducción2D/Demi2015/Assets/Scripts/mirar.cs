d56250ddf9a096e282a75dbda50f30eaf72f5c77
Pedro Marín Ramos <pmarinr@gmail.com>
2015-10-12 22:09:25 +0300

    TeleTransporte

---
+++ Introducción2D/Demi2015/Assets/Scripts/mirar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+// The GameObject requires a Rigidbody component
+[RequireComponent(typeof(Transform))]
+public class mirar : MonoBehaviour {
+    public Transform target;
+    private Vector3 v_diff;
+    private float atan2;
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        v_diff = (target.position - transform.position);
+        atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg);
+        Debug.Log(transform.localRotation.eulerAngles.z);
+        if(transform.localRotation.eulerAngles.z > 90 && transform.localRotation.eulerAngles.z < 280)
+        {
+            transform.localScale = new Vector3(-1,-1,1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
---
+++ Introducción2D/Demi2015/Assets/Scripts/teleport.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleport : MonoBehaviour {
+    public GameObject efecto;
+
+    private Transform target;
+
+    void Awake()
+    {
+        target = transform.Find("target");
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject e = (GameObject)Instantiate(efecto, col.transform.position, col.transform.rotation);
+        //Destroy(e, 1);
+        col.transform.position = target.transform.position;
+        col.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        e = (GameObject)Instantiate(efecto, col.transform.position, col.transform.rotation);
+        //Destroy(e, 1);
+    }
+    void OnDrawGizmos()
+     {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, target.position);
+
+    }
+}
