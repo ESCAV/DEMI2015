ec60eae77ccb5272eb6f519a226b9baa481cd3ab
Pedro Marín Ramos <pmarinr@gmail.com>
2015-12-18 15:34:47 +0100

    Level1

---
+++ Introducción2D/Demi2015/Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyIA : MonoBehaviour {
+    public enum Estado {parado, andando, persiguiendo }
+
+    public Estado estado;
+    // Use this for initialization
+    void Start () {
+        estado = Estado.parado;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
